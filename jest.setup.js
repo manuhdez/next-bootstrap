7676ff005aef9baca9e8dff14266e8df0af657c0
Manuel Hernández <hi@manuhdez.com>
2022-02-15 20:06:16 +0000

    test: jest setup

---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+// Used to extend jest dom matchers. Like: `expect(element).toBeInTheDocument()`
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect'
---
+++ pages/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Page from './index';
+
+describe("index page", () => {
+  test("can see the welcome message", () => {
+    render(<Page />);
+    const title = screen.getByRole("heading", { name: /welcome/i })
+    expect(title).toBeInTheDocument();
+  });
+})
