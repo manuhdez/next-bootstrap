6ee82e87404e99598ce7ac83a52e8cfb33fad378
Manuel Hernández <hi@manuhdez.com>
2022-02-16 15:59:28 +0000

    style: run prettier script

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,6 @@ const Home: NextPage = () => {
             <button onClick={decrement}>Decrement</button>
           </div>
         </div>
-
       </main>
 
       <footer className={styles.footer}>
